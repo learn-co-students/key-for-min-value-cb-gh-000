aad32cd95dd47bc29713ad7fbb4bfe127ad47312
Sam Ebertz <samuel.ebertz@gmail.com>
2017-05-30 18:34:28 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+FIXNUM_MAX = (2**(0.size * 8 -2) -1)
 def key_for_min_value(name_hash)
-
+  k = nil
+  min = FIXNUM_MAX
+  name_hash.each do |key, value|
+    k,min = key,value if value < min
+  end
+  return k
 end
