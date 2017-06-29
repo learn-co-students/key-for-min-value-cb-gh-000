0f65f6eb8073ad0c93211900b95f18208fe52445
Leslie Huang <lesliehuang@gmail.com>
2017-06-29 01:49:07 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,20 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash == {}
+    return nil
+
+  else
+
+    lowest_key, lowest_val = name_hash.first
+
+    name_hash.each do |key, val|
+      if name_hash[key] < lowest_val
+        lowest_key = key
+      end
+
+    end
+
+    return lowest_key
+  end
 end
