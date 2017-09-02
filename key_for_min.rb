75d20ba2cccaf0e4c4ca648a22c8c0b84662af1c
Melissa Sheehan <mxsheehan7@gmail.com>
2017-09-02 22:54:27 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  minKey = nil
+  minValue = nil
 
+  name_hash.each do |key, value|
+    if minValue == nil || value < minValue #checks to see if minValue is nil FIRST (doesn't check the second statement is this is true )
+      # if value < minValue || minValue == nil if you do this order, you might try to compare a value to nil (errors)
+      minValue = value
+      minKey = key
+    end
+  end
+  return minKey
 end
+
+# hash = {:blake => 500, :ashley => 2, :adam => 1}
+# key_for_min_value(hash)
