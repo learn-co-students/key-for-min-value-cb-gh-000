ee9ae169b31a6273c60737af072f17fe71338462
Kaspar JohannesPärtel <kasparjohannes.partel@tptlive.ee>
2018-02-19 17:39:07 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_v = nil;
+  min_k = nil;
+  name_hash.each do |k, v|
+    if min_v == nil
+      min_v = v
+      min_k = k
+    elsif min_v > v
+      min_v = v
+      min_k = k
+    elsif name_hash == {}
+      return nil
+    end
+  end
+  min_k
 end
