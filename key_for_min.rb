5bba9a81c6153d7cb2efdba2b30a6b63ed96e14c
Plamen Dzhelepov <plamen.gj@gmail.com>
2018-03-05 15:51:17 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,16 +1,11 @@
-def key_for_min_value(name_hash)
-current_minimum_val = nil
-current_key_assoc_w_min_val = nil
-   name_hash.each do |key, value|
-    if current_minimum_val == nil
-      current_minimum_val = value
-     current_key_assoc_w_min_val = key
-   else
-      if current_minimum_val > value
-       current_minimum_val = value
-        current_key_assoc_w_min_val = key
-      end
-   end
- end
-  current_key_assoc_w_min_val
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
 end
