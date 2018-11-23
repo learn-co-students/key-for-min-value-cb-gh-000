af9ec48477ba59023ea8401ec59b052ca07f36c0
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 14:22:57 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.size == 0
+    return nil
+  else
+    min_hash_value = 10 ** 80
+    min_hash_key = -1
+    name_hash.collect do |k, v|
+      min_hash_value, min_hash_key = v, k if v < min_hash_value
+    end
 
+  end
+  return min_hash_key
 end
