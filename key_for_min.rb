19e07f2ae64513ee135613d42c29096026a8f34a
Sam Json <3115002443@mail2.gdut.edu.cn>
2018-12-14 04:12:40 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.length > 0
+    first_hash = name_hash.shift
+    _min = first_hash[1]
+    _name = first_hash[0]
+    name_hash.each do |name,value|
+      if value < _min
+        _min = value
+        _name = name
+      end
+    end
+    _name
+  end
 
 end
